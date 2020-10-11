43db60f44ac5d09ad3272aeb490877315569dd6d
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-11 20:51:43 -0300

    DatabaseManager creado

--- src/persistencia/DatabaseManager.java
+++ src/persistencia/DatabaseManager.java
@@ -1,5 +1,22 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
+	
+	private static Connection Conexion;
+	private static String CADENA_CONEXION = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+	private static String USUARIO = "JAVA";
+	private static String CLAVE = "JAVA";
+	
+	static {
+		try {
+			Conexion = DriverManager.getConnection(CADENA_CONEXION,USUARIO, CLAVE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
