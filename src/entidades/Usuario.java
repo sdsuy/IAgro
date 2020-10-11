23d58a660bcd01dc3815eb019a7f31ee45dfdf1f
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-11 11:32:18 -0300

    Creado Usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,5 +1,66 @@
 package entidades;
 
 public abstract class Usuario {
+	
+	private int id_usuario;
+	private String nombre;
+	private String apellido;
+	private String user;
+	private String pswd;
+	private String email;
+	
+	public Usuario(int id_usuario, String nombre, String apellido, String user, String pswd, String email) {
+		super();
+		this.id_usuario = id_usuario;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.user = user;
+		this.pswd = pswd;
+		this.email = email;
+	}
+	
+	public Usuario() {
+		super();
+	}
+
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 
 }
