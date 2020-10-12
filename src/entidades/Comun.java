44ee9ffb76feecc0ea2e6d562affa59c724afdd4
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-12 13:35:06 -0300

    Creada la clase común

--- src/entidades/Comun.java
+++ src/entidades/Comun.java
@@ -1,5 +1,25 @@
 package entidades;
 
 public class Comun extends Usuario {
+	
+	private String list_tarea;
+
+	public Comun() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Comun(int id_usuario, String nombre, String apellido, String user, String pswd, String email) {
+		super(id_usuario, nombre, apellido, user, pswd, email);
+		
+	}
+
+	public String getList_tarea() {
+		return list_tarea;
+	}
+
+	public void setList_tarea(String list_tarea) {
+		this.list_tarea = list_tarea;
+	}	
 
 }
