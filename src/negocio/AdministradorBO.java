38f39c585d72227f649fbc08cbef755acb2dd3f9
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-14 11:34:57 -0300

    Creado Business Object de Administrador.

--- src/negocio/AdministradorBO.java
+++ src/negocio/AdministradorBO.java
@@ -1,5 +1,92 @@
 package negocio;
 
+import java.util.LinkedList;
+
+import entidades.Administrador;
+import persistencia.DAOAdministrador;
+
 public class AdministradorBO {
+	
+	private LinkedList<Administrador> admins;
+	private Administrador admin;
+	
+	//Constructor que crea un linkedlist de personas vacio
+	public AdministradorBO() {
+		this.admins = new LinkedList<>();
+	}
+	
+	/**
+	 * 
+	 * Agrego un administrador
+	 * @param admin
+	 * @return
+	 */
+	public boolean agregarAdmin(Administrador adm) {
+		
+		return DAOAdministrador.nuevoUsuario(adm);
+		
+	}
+	
+	//Cargo los Administradores
+	public void obtenerAdmins() {
+		
+		admins = DAOAdministrador.selectUser();
+	}
+	
+	/**
+	 * 
+	 * Actualizo un administrador
+	 * @param adm
+	 * @return
+	 */
+	public boolean actualizarAdmin(Administrador adm) {
+		
+		return DAOAdministrador.updateUser(adm);
+	
+	}
+	
+	/**
+	 * 
+	 * Elimino un Administrador
+	 * @param idAdmin
+	 * @return
+	 */
+	public boolean eliminarAdmin(int idUser) {
+		
+		return DAOAdministrador.deleteUser(idUser);
+	}
+	
+	/**
+	 * 
+	 * Getter del linkedlist Admins
+	 * @return
+	 */
+	public LinkedList<Administrador> getPersonas() {
+		
+		return admins;
+	}
+	
+	/**
+	 * Busco un administrador
+	 * @param cedula
+	 */
+	
+	public void buscarAdmin(int cedula) {
+		
+		admin = DAOAdministrador.findUser(cedula);
+		
+	}
+	
+	/**
+	 * Getter de Administrador
+	 * @return
+	 */
+	public Administrador getPersona() {
+		return admin;
+		
+	}
+	
+
+
 
 }
