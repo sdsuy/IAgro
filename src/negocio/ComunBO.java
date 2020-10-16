17d9248b0955c434e174a99131c514b1ab51fe95
mauricio_bellon <mauricio.bellon@estudiantes.utec.edu.uy>
2020-10-15 23:46:48 -0300

    en progreso

--- src/negocio/ComunBO.java
+++ src/negocio/ComunBO.java
@@ -1,5 +1,41 @@
 package negocio;
 
-public class ComunBO {
+import java.util.LinkedList;
 
+import entidades.Comun;
+import persistencia.DAOComun;
+
+public class ComunBO {
+	private LinkedList<Comun> comunes;
+	//constructor
+	public ComunBO(){
+		comunes = DAOComun.listarUsuarios();	
+	}
+	
+	//insertar nuevo usuario comun
+	public void insertarUsuario(Comun usuario){
+		DAOComun.nuevoUsuario(usuario);
+		System.out.println("Usuario ingresado");
+	}
+	
+	//modificar usuario Comun
+	public void modificarComun(){
+		//
+	}
+	//eliminar usuario Comun
+	public void eliminarComun(Comun usuario){
+		DAOComun.borrarUsuario(usuario);
+		System.out.println("Usuario compún "+usuario.getUser()+"eliminado");
+	}
+	
+	//obtener lista usuario Comun
+	public LinkedList<Comun> listarComun(){
+		return DAOComun.listarUsuarios();
+	}
+	
+	//encontrar usuario por nombre de usuario
+	public Comun encontrarUsuario(String nombreUsuario){
+		return DAOComun.encontrarUsuario(nombreUsuario);
+	}
+	
 }
