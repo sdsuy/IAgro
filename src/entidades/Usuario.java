92ba4b486ff46d88aa8b1faec7986841ad07ba66
Santiago Da Silva <sdasilva@outlook.es>
2020-10-16 14:06:06 -0300

    simplifico el nombre del campo id

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -2,7 +2,7 @@ package entidades;
 
 public abstract class Usuario {
 	
-	private int id_usuario;
+	private int id;
 	private String nombre;
 	private String apellido;
 	private String user;
@@ -10,12 +10,12 @@ public abstract class Usuario {
 	private String email;
 	
 	public Usuario() {
-		super();
 		
 	}
-	public Usuario(int id_usuario, String nombre, String apellido, String user, String pswd, String email) {
+
+	public Usuario(int id, String nombre, String apellido, String user, String pswd, String email) {
 		super();
-		this.id_usuario = id_usuario;
+		this.id = id;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.user = user;
@@ -23,44 +23,52 @@ public abstract class Usuario {
 		this.email = email;
 	}
 
-	public int getId_usuario() {
-		return id_usuario;
+	public int getId() {
+		return id;
 	}
-	public void setId_usuario(int id_usuario) {
-		this.id_usuario = id_usuario;
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public String getPswd() {
 		return pswd;
 	}
+
 	public void setPswd(String pswd) {
 		this.pswd = pswd;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-	
 
 }
