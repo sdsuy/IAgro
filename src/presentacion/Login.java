f5b0f99357787d904ae59db73de854936a3f6b8d
WildIndex <carlossuarez464@gmail.com>
2020-10-16 21:48:03 -0300

    Hecho el log in

--- src/presentacion/Login.java
+++ src/presentacion/Login.java
@@ -5,12 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 import negocio.AdministradorBO;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+import javax.swing.JTextField;
+import java.awt.Font;
 
 public class Login {
 
-	private JFrame frame;
+	private JFrame frmIagro;
 	
 	private AdministradorBO administrador = new AdministradorBO();
+	private JTextField txtFieldUser;
+	private JTextField textPass;
 
 	/**
 	 * Launch the application.
@@ -20,7 +28,7 @@ public class Login {
 			public void run() {
 				try {
 					Login window = new Login();
-					window.frame.setVisible(true);
+					window.frmIagro.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,9 +50,28 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmIagro = new JFrame();
+		frmIagro.setTitle("IAgro");
+		frmIagro.setBounds(100, 100, 750, 500);
+		frmIagro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frmIagro.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblFondo.setIcon(new ImageIcon(Login.class.getResource("/IMG/login.png")));
+		lblFondo.setBounds(0, 0, 734, 461);
+		desktopPane.add(lblFondo);
+		
+		txtFieldUser = new JTextField();
+		txtFieldUser.setBounds(324, 225, 143, 34);
+		desktopPane.add(txtFieldUser);
+		txtFieldUser.setColumns(10);
+		
+		textPass = new JTextField();
+		textPass.setColumns(10);
+		textPass.setBounds(324, 288, 143, 34);
+		desktopPane.add(textPass);
 	}
-
 }
