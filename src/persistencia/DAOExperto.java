9b7a84e07e1ded0852e0e310f949b5938c05cdee
WildIndex <carlossuarez464@gmail.com>
2020-10-17 21:24:46 -0300

    primer metodo de daoexperto

--- src/persistencia/DAOExperto.java
+++ src/persistencia/DAOExperto.java
@@ -1,5 +1,30 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import entidades.Comun;
+import entidades.Experto;
+
 public class DAOExperto {
+	
+	private static Connection conexion = DatabaseManager.getConexion();
+	
+	public static boolean nuevoUsuario(Experto usuario) {
+		String consulta = "INSERT INTO EXPERTOS (ID_USUARIO, LIST_TAREAS, CEDULA, PROFESION, NOMBRE, APELLIDO, NOMB_USUARIO, CONTRASENIA, EMAIL) "
+				+ "VALUES (SEQ_ID_USUARIO.NEXT_VAL,?,?,?,?,?,?,?,?)";
+		try {
+			Statement sentencia = conexion.createStatement();
+			sentencia.executeQuery(consulta);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
 
 }
