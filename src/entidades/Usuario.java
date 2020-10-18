7b087786e4e95f1c4992104466fb8ea685faa97e
Santiago Da Silva <sdasilva@outlook.es>
2020-10-18 16:10:19 -0300

    Generalizo la capa entidades segun todos los atributos posibles de Usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -9,11 +9,19 @@ public abstract class Usuario {
 	private String pswd;
 	private String email;
 	
+	private String profesion; // usuario experto
+	
+	private int cedula; // usuario experto y administrador
+	private String list_tareas; // usuario experto y administrador
+	
+	private String instituto; // usuario administrador
+
 	public Usuario() {
-		
+		super();
 	}
 
-	public Usuario(int id, String nombre, String apellido, String user, String pswd, String email) {
+	public Usuario(int id, String nombre, String apellido, String user, String pswd, String email, String profesion,
+			int cedula, String list_tareas, String instituto) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -21,6 +29,10 @@ public abstract class Usuario {
 		this.user = user;
 		this.pswd = pswd;
 		this.email = email;
+		this.profesion = profesion;
+		this.cedula = cedula;
+		this.list_tareas = list_tareas;
+		this.instituto = instituto;
 	}
 
 	public int getId() {
@@ -69,6 +81,38 @@ public abstract class Usuario {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getProfesion() {
+		return profesion;
+	}
+
+	public void setProfesion(String profesion) {
+		this.profesion = profesion;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getList_tareas() {
+		return list_tareas;
+	}
+
+	public void setList_tareas(String list_tareas) {
+		this.list_tareas = list_tareas;
+	}
+
+	public String getInstituto() {
+		return instituto;
+	}
+
+	public void setInstituto(String instituto) {
+		this.instituto = instituto;
 	}
 
 }
