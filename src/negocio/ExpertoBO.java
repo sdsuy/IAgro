c54da1d8964e73091ce422e53d3d6ad90f40fee2
WildIndex <carlossuarez464@gmail.com>
2020-10-18 20:07:17 -0300

    creado metodo createUsuario en ExpertoBO

--- src/negocio/ExpertoBO.java
+++ src/negocio/ExpertoBO.java
@@ -3,18 +3,39 @@ package negocio;
 import java.util.LinkedList;
 
 import entidades.Usuario;
+import persistencia.DAOExperto;
 
 public class ExpertoBO extends UsuarioBO {
-
+	
+	
+	private LinkedList<Usuario> experts;
+	
+	//linkedlist expertos
+	public ExpertoBO() {
+			this.experts = new LinkedList<>();
+	}
+	
+	/**
+	 * 
+	 * Agrego un Experto
+	 * @param Usuario
+	 * @return
+	 */
+	
 	@Override
 	public boolean createUsuario(Usuario usuario) {
-		// TODO Auto-generated method stub
-		return false;
+		return DAOExperto.nuevoUsuario(usuario);
 	}
+	
+	/**
+	 * 
+	 * Busco un Usuario
+	 * @param int
+	 * @return Usuario
+	 */
 
 	@Override
 	public Usuario findUsuario(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
