32ea3825dda74fc86fb878e4391d35cbaea0139c
Santiago Da Silva <sdasilva@outlook.es>
2020-10-18 18:12:56 -0300

    implemento los metodos que deberan ser escritos

--- src/negocio/ExpertoBO.java
+++ src/negocio/ExpertoBO.java
@@ -1,5 +1,33 @@
 package negocio;
 
-public class ExpertoBO {
+import java.util.LinkedList;
+
+import entidades.Usuario;
+
+public class ExpertoBO extends UsuarioBO {
+
+	@Override
+	public boolean createUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Usuario findUsuario(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<Usuario> allUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
