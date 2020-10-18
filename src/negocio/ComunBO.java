3e8f3d80f85fa1837d68532ceecf6a758fefe01e
Santiago Da Silva <sdasilva@outlook.es>
2020-10-18 18:14:06 -0300

    se implementan los metodos que deberan ser escritos, dejo comentadas las lineas que desactualizadas

--- src/negocio/ComunBO.java
+++ src/negocio/ComunBO.java
@@ -3,14 +3,15 @@ package negocio;
 import java.util.LinkedList;
 
 import entidades.Comun;
+import entidades.Usuario;
 import persistencia.DAOComun;
 
-public class ComunBO {
+public class ComunBO extends UsuarioBO {
 	private LinkedList<Comun> comunes;
 	
 	//constructor
-	public ComunBO(){
-		comunes = DAOComun.listarUsuarios();	
+	public ComunBO() {
+//		comunes = DAOComun.listarUsuarios();
 	}
 	
 	//insertar nuevo usuario comun
@@ -21,12 +22,12 @@ public class ComunBO {
 	
 	//modificar usuario Comun
 	public void modificarComun(Comun usuario){
-		DAOComun.updateUsuario(usuario);
+//		DAOComun.updateUsuario(usuario);
 		System.out.println("Modificando usuario");
 	}
 	//eliminar usuario Comun
 	public void eliminarComun(Comun usuario){
-		DAOComun.borrarUsuario(usuario);
+//		DAOComun.borrarUsuario(usuario);
 		System.out.println("Usuario compún "+usuario.getUser()+"eliminado");
 	}
 	
@@ -37,7 +38,32 @@ public class ComunBO {
 	
 	//encontrar usuario por nombre de usuario
 	public Comun encontrarUsuario(String nombreUsuario){
-		return DAOComun.encontrarUsuario(nombreUsuario);
+//		return DAOComun.encontrarUsuario(nombreUsuario);
+		return null;
+	}
+
+	@Override
+	public boolean createUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Usuario findUsuario(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<Usuario> allUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
