c9f5c61f14a3d9197bddc477b30fdb6cfc037f24
WildIndex <carlossuarez464@gmail.com>
2020-10-17 21:09:21 -0300

    Creado menu admin

--- src/presentacion/VentanaPrincipal.java
+++ src/presentacion/VentanaPrincipal.java
@@ -3,6 +3,17 @@ package presentacion;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JMenu;
 
 public class VentanaPrincipal {
 
@@ -36,8 +47,43 @@ public class VentanaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 750, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 734, 29);
+		desktopPane.add(menuBar);
+		
+		JMenu mnAdministrador = new JMenu("Administrador");
+		menuBar.add(mnAdministrador);
+		
+		JMenuItem mntmAlta = new JMenuItem("Alta");
+		mnAdministrador.add(mntmAlta);
+		
+		JMenuItem mntmBaja = new JMenuItem("Baja");
+		mnAdministrador.add(mntmBaja);
+		
+		JMenuItem mntmModificacion = new JMenuItem("Modificacion");
+		mnAdministrador.add(mntmModificacion);
 	}
-
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
