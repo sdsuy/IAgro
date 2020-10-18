5cb17a283ebbebcf0a6bd9b6d1e3b49837788a68
Santiago Da Silva <sdasilva@outlook.es>
2020-10-17 22:41:57 -0300

    agrego un metodo para login que devuelve el id del usuario logueado

--- src/persistencia/DAOUsuario.java
+++ src/persistencia/DAOUsuario.java
@@ -2,14 +2,17 @@ package persistencia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class DAOUsuario {
 	
-	protected static final String INSERT_USUARIO = "INSERT INTO USUARIOS (ID_USUARIO, NOMBRE, APELLIDO, NOMB_USUARIO, CONTRASENIA, EMAIL) VALUES (SEQ_ID_USUARIO.NEXTVAL,?,?,?,?,?)";
-	protected static final String UPDATE_USUARIO = "UPDATE USUARIOS SET NOMBRE=?,APELLIDO=?,NOMB_USUARIO=?,CONTRASENIA=?,EMAIL=? WHERE ID_USUARIO=?";
+	protected static final String INSERT_USUARIO = "INSERT INTO usuarios (ID_USUARIO, NOMBRE, APELLIDO, NOMB_USUARIO, CONTRASENIA, EMAIL) VALUES (SEQ_ID_USUARIO.NEXTVAL,?,?,?,?,?)";
+	protected static final String UPDATE_USUARIO = "UPDATE usuarios SET NOMBRE=?,APELLIDO=?,NOMB_USUARIO=?,CONTRASENIA=?,EMAIL=? WHERE ID_USUARIO=?";
 	
-	private static final String DELETE_USUARIO = "DELETE FROM USUARIOS WHERE ID_USUARIO = ?";
+	private static final String DELETE_USUARIO = "DELETE FROM usuarios WHERE id_usuario = ?";
+	private static final String LOGIN = "SELECT id_usuario FROM usuarios WHERE nomb_usuario=? AND contrasenia=?";
+	
 	private static Connection conexion = DatabaseManager.getConexion();
 	
 	public static boolean deleteUser(int id) {
@@ -28,7 +31,21 @@ public abstract class DAOUsuario {
 			e.printStackTrace();
 		}
 		return false;
-		
+	}
+	
+	public static int login(String username, String password) {
+		int id = 0;
+		try {
+			PreparedStatement pst = conexion.prepareStatement(LOGIN);
+			pst.setString(1, username);
+			pst.setString(2, password);
+			
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()) id = rs.getInt("ID_USUARIO");			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return id;
 	}
 
 }
