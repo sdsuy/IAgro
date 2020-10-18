00db1ec8b3fc7d1083023f9bcdd8dcb5cab1d17f
WildIndex <carlossuarez464@gmail.com>
2020-10-17 21:31:26 -0300

    no message

--- src/persistencia/DAOExperto.java
+++ src/persistencia/DAOExperto.java
@@ -9,21 +9,7 @@ import entidades.Experto;
 
 public class DAOExperto {
 	
-	private static Connection conexion = DatabaseManager.getConexion();
 	
-	public static boolean nuevoUsuario(Experto usuario) {
-		String consulta = "INSERT INTO EXPERTOS (ID_USUARIO, LIST_TAREAS, CEDULA, PROFESION, NOMBRE, APELLIDO, NOMB_USUARIO, CONTRASENIA, EMAIL) "
-				+ "VALUES (SEQ_ID_USUARIO.NEXT_VAL,?,?,?,?,?,?,?,?)";
-		try {
-			Statement sentencia = conexion.createStatement();
-			sentencia.executeQuery(consulta);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-	}
 	
 	
 
