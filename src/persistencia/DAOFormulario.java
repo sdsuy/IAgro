5c65b6841d3458de9ba0617386172b873dc51e51
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-22 14:22:19 -0300

    Creo metodo updateForm

--- src/persistencia/DAOFormulario.java
+++ src/persistencia/DAOFormulario.java
@@ -10,7 +10,7 @@ public class DAOFormulario {
 	private static Connection conexion = DatabaseManager.getConexion();
 	
 	private static final String INSERT_FORMULARIO = "INSERT INTO FORMULARIO (ID_FORMULARIO,ID_USUARIO,MET_MUESTREO,EQUIPAMIENTO,NOM_FORMULARIO,"
-			+ "RESUMEN,DEPARTAMENTO,FECHA,ZONA,TIP_MUESTREO,GEOPUNTO,LOCALIDAD,EST_MUESTREO) VALUES (SEQ_ID_FORMULARIO.NEXTVAL,SEQ_ID_USUARIO.CURRVAL,?,?,?,?,?,?,?,?,?,?,?)";
+			+ "RESUMEN,DEPARTAMENTO,FECHA,ZONA,TIP_MUESTREO,GEOPUNTO,LOCALIDAD,EST_MUESTREO) VALUES (SEQ_ID_FORMULARIO.NEXTVAL,?,?,?,?,?,?,?,?,?,?,?,?)";
 	private static final String DELETE_FORMULARIO = "DELETE FROM FORMULARIO SET ID_FORMULARIO=?";
 	private static final String UPDATE_FORMULARIO = "UPDATE FORMULARIO SET MET_MUESTREO = ?,EQUIPAMIENTO = ?,NOM_FORMULARIO = ?,\"\n"
 			+ "			+ \"RESUMEN = ?,DEPARTAMENTO = ?,FECHA = ?,ZONA = ?,TIP_MUESTREO = ?,GEOPUNTO = ?,LOCALIDAD = ?,EST_MUESTREO = ? WHERE ID_FORMULARIO = ?";
@@ -23,17 +23,19 @@ public class DAOFormulario {
 		try {
 			PreparedStatement insertForm = conexion.prepareStatement(INSERT_FORMULARIO);
 			
-			insertForm.setString(1, formulario.getMet_muestreo());
-			insertForm.setString(2, formulario.getEquipamiento());
-			insertForm.setString(3, formulario.getNom_formulario());
-			insertForm.setString(4, formulario.getResumen());
-			insertForm.setString(5, formulario.getDepartamento());
-			insertForm.setDate(6, formulario.getFecha());
-			insertForm.setString(7, formulario.getZona());
-			insertForm.setString(8, formulario.getTip_muestreo());
-			insertForm.setLong(9, formulario.getGeopunto());
-			insertForm.setString(10, formulario.getLocalidad());
-			insertForm.setString(11, formulario.getEst_muestreo());
+			insertForm.setInt(1, formulario.getUser().getId());
+			insertForm.setString(2, formulario.getMet_muestreo());
+			insertForm.setString(3, formulario.getEquipamiento());
+			insertForm.setString(4, formulario.getNom_formulario());
+			insertForm.setString(5, formulario.getResumen());
+			insertForm.setString(6, formulario.getDepartamento());
+			insertForm.setDate(7, formulario.getFecha());
+			insertForm.setString(8, formulario.getZona());
+			insertForm.setString(9, formulario.getTip_muestreo());
+			insertForm.setLong(10, formulario.getGeopunto());
+			insertForm.setString(11, formulario.getLocalidad());
+			insertForm.setString(12, formulario.getEst_muestreo());
+			
 			
 			int filasAgregadas = insertForm.executeUpdate();
 			
@@ -45,7 +47,7 @@ public class DAOFormulario {
 		
 	}
 	
-	public static boolean eliminarFormulario(int id) {
+	public static boolean deleteFormulario(int id) {
 		
 		try {
 			PreparedStatement elimForm = conexion.prepareStatement(DELETE_FORMULARIO);
@@ -62,4 +64,34 @@ public class DAOFormulario {
 		
 	}
 	
+	public static boolean updateForm(Formulario formulario) {
+		
+		try {
+			PreparedStatement update = conexion.prepareStatement(UPDATE_FORMULARIO);
+			
+			
+			update.setString(1, formulario.getMet_muestreo());
+			update.setString(2, formulario.getEquipamiento());
+			update.setString(3, formulario.getNom_formulario());
+			update.setString(4, formulario.getResumen());
+			update.setString(5, formulario.getDepartamento());
+			update.setDate(6, formulario.getFecha());
+			update.setString(7, formulario.getZona());
+			update.setString(8, formulario.getTip_muestreo());
+			update.setLong(9, formulario.getGeopunto());
+			update.setString(10, formulario.getLocalidad());
+			update.setString(11, formulario.getEst_muestreo());
+			
+			
+			int filasAgregadas = update.executeUpdate();
+			
+			return filasAgregadas>0;
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
 }
