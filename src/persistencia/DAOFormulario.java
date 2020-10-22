921f06b64def4b8e7f6c28e78b311ca01ca8f1ba
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-22 15:47:30 -0300

    Elimino import

--- src/persistencia/DAOFormulario.java
+++ src/persistencia/DAOFormulario.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
-
-import apple.laf.JRSUIConstants.State;
 import entidades.Formulario;
 import entidades.Usuario;
 
