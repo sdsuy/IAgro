c1f87051523b5d248e5c89a2cdd1841364cf2211
Santiago Da Silva <sdasilva@outlook.es>
2020-10-22 02:15:03 -0300

    integro presentacion de listado de usuarios a IAgro

--- src/aplicacion/IAgro.java
+++ src/aplicacion/IAgro.java
@@ -9,6 +9,7 @@ import negocio.ExpertoBO;
 import negocio.UsuarioBO;
 import presentacion.Login;
 import presentacion.Registro;
+import presentacion.TablaUsuarios;
 import presentacion.VentanaPrincipal;
 
 public class IAgro {
@@ -16,6 +17,7 @@ public class IAgro {
 	private Login login; // capa presentacion de login
 	private VentanaPrincipal ventana; // capa presentacion de ventana principal
 	private Registro registro; // capa presentacion de registro
+	private TablaUsuarios listado; // capa presentacion de listado
 	private UsuarioBO usuario_; // capa de negocios de usuario logueado
 	private UsuarioBO userService; // capa de negocios para gestionar los usuarios
 	private Usuario usuario; // usuario logueado
@@ -27,37 +29,34 @@ public class IAgro {
 		iagro.start();
 	}
 	
+	/**
+	 * 
+	 * Constructor de IAgro con la UI inicial de login sin mostrarla
+	 */
 	public IAgro() {
 		login = new Login(this); // Inyecto esta clase a login
 	}
 	
+	/**
+	 * 
+	 * Metodo para arrancar IAgro con la capa de negocios inicial y mostrando login
+	 */
 	private void start() {
 		usuario_ = new AdministradorBO(); // instancia la capa de negocio.
 		refreshUsuarios();
 		// si es el primer uso creamos un usuario administrador:
 		if(usuarios.size() < 1) if(usuario_.bootstrap()) login.mensajeEditarAdminPassword();
-		login.start(); // muestro la ventana de login
+		login.start(); // muestro la ventana de login con al menos 1 usuario por defecto cargado en la BD
 	}
 	
-	public void login(String username, String password) {
-		id = usuario_.login(username, password);
-	}
-
+	//***********************************************
+	// Metodos para el manejo de la capa presentacion
+	//***********************************************
+	
 	public int getId() {
 		return id;
 	}
 	
-	public void findUsuario(){
-		usuario = usuario_.findUsuario(id); // lo busco como administrador
-		if(!(usuario.getId() > 0)) {
-			usuario_ = new ExpertoBO(); // si no encuentra lo busco como experto
-			usuario = usuario_.findUsuario(id);
-		} else if(!(usuario.getId() > 0)) {
-			usuario_ = new ComunBO(); // si no encuentra lo busco como comun
-			usuario = usuario_.findUsuario(id);
-		}
-	}
-
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -71,9 +70,38 @@ public class IAgro {
 		registro = new Registro(this); // instancio la ventana de registro al hacer click en Alta en el menu Usuario
 		registro.start();
 	}
-
+	
+	public void listado() {
+		listado = new TablaUsuarios(this); // instancio la ventana de listado al hacer click en Listar en el menu Usuario
+		listado.start();
+	}
+	
 	public LinkedList<Usuario> getUsuarios() {
 		return usuarios;
+	}
+	
+	public String[] getColumnas() {
+		String[] columnas = { "ID", "NOMBRE", "APELLIDO", "USUARIO", "CONTRASENIA", "E-MAIL", "CEDULA", "INSTITUTO", "PROFESION", "LISTA DE TAREAS", "ROL"};
+		return columnas;
+	}
+	
+	//**********************************************
+	// Metodos para el manejo de la capa de negocios
+	//**********************************************
+	
+	public void login(String username, String password) {
+		id = usuario_.login(username, password);
+	}
+	
+	public void findUsuario(){
+		usuario = usuario_.findUsuario(id); // lo busco como administrador
+		if(!(usuario.getId() > 0)) {
+			usuario_ = new ExpertoBO(); // si no encuentra lo busco como experto
+			usuario = usuario_.findUsuario(id);
+		} else if(!(usuario.getId() > 0)) {
+			usuario_ = new ComunBO(); // si no encuentra lo busco como comun
+			usuario = usuario_.findUsuario(id);
+		}
 	}
 	
 	public void refreshUsuarios() {
@@ -90,5 +118,7 @@ public class IAgro {
 		}
 		return userService.createUsuario(usuario);
 	}
+	
+	
 
 }
